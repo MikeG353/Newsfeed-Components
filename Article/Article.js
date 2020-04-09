280500d832d84aaeb7587fe27d790a7b98856249
Michael Gregory <michael.gregory8584@gmail.com>
2020-04-09 16:54:05 -0500

    Project Complete

--- Article/Article.js
+++ Article/Article.js
@@ -85,30 +85,75 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'test',
+    date: '04/09/2020',
+    firstParagraph: 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ',
+    secondParagraph: 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test ',
+    thirdParagraph: 'test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test test '
+
   }
 ];
 
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
+// Step 1: Create a function that creates a component. You will want your component to look like the template below: 
   
-  <div class="article">
+function makeArticle({ title, date, firstParagraph, secondParagraph, thirdParagraph }) {
+  //create elements
+  const articleDiv = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const articleFirstParagraph = document.createElement('p');
+  const articleSecondParagraph = document.createElement('p');
+  const articleThirdParagraph = document.createElement('p');
+  const articleExpandButton = document.createElement('span');
+
+  
+//structure
+  articleDiv.appendChild(articleTitle);
+  articleDiv.appendChild(articleDate);
+  articleDiv.appendChild(articleFirstParagraph);
+  articleDiv.appendChild(articleSecondParagraph);
+  articleDiv.appendChild(articleThirdParagraph);
+  articleDiv.appendChild(articleExpandButton);
+
+  
+  // add classes
+
+  articleDiv.classList.add('article');
+  articleDate.classList.add('date');
+  articleExpandButton.classList.add('expandButton');
+
+
+  // add content
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  articleFirstParagraph.textContent = firstParagraph;
+  articleSecondParagraph.textContent = secondParagraph;
+  articleThirdParagraph.textContent = thirdParagraph;
+  articleExpandButton.textContent = 'expand/collapse';
+
+  
+  
+  /*<div class="article">
     <h2>{title of the article}</h2>
     <p class="date">{date of the article}</p>
 
     {three separate paragraph elements}
 
     <span class='expandButton'></span>
-  </div>
+  </div> */
+  //Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
+  articleExpandButton.addEventListener('click', (event) => {articleDiv.classList.toggle('article-open')});
+  
+  //Step 3: return the entire component.
+  return articleDiv;
+}
 
-  Hint: You will need to use createElement more than once here!
+//Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
+let articleData = data.map(data => makeArticle(data));
 
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
+let article = document.querySelector('.articles')
+articleData.forEach(articleDiv => article.appendChild(articleDiv))
 
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-  Step 3: return the entire component.
-
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
-*/
+ // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,27 +9,47 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+//  Step 1: Write a function that will create a menu component as seen below:
+function makeMenu() {
 
-  Step 1: Write a function that will create a menu component as seen below:
-
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
-
-  The function takes an array as its only argument.
-
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
-
-  Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
-
-  Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
   
-*/
+  // <div class="menu">
+  //   <ul>
+  //     {each menu item as a list item}
+  //   </ul>
+  // </div>
+
+  // create elements
+  const menu = document.createElement('div');
+  const menuList = document.createElement('ul');
+  // const menuItem = document.createElement('li');
+  // add class to div
+  menu.classList.add('menu');
+  
+  //structure
+  menu.appendChild(menuList)
+  // Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+  // Add those items to the <ul>
+  menuItems.forEach(element => { 
+    const menuItem = document.createElement('li');
+    menuItem.textContent = element;
+    menuList.appendChild(menuItem);
+    });
+  
+  // The function takes an array as its only argument. 
+
+  // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
+  let menuButton = document.querySelector('.menu-button');
+  // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  menuButton.addEventListener('click', (event) => {menu.classList.toggle('menu--open')});
+
+  // Step 5: return the menu component.
+
+return menu;
+
+}
+
+  // Step 6: add the menu component to the DOM.
+  let header = document.querySelector('.header');
+  header.appendChild(makeMenu(menuItems));
+
